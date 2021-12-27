4cf10ec7ef2e3bd00d0174c77d27ca7b48a52d2e
GarudaProjects <80512930+GarudaProjects@users.noreply.github.com>
2021-12-27 18:41:37 +0700

    Update ready.js

--- events/discord/ready.js
+++ events/discord/ready.js
@@ -12,7 +12,7 @@ module.exports = async (client) => {
   console.log(`[ / | Slash Command ] - ✅ Loaded all slash commands!`)
   let invite = `https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=applications.commands%20bot`;
   console.log(`[STATUS] ${client.user.tag} is now online!\n[INFO] Bot by ZeroSync https://www.youtube.com/c/ZeroSync\n[Invite Link] ${invite}`);
-  const activities = [`All Giveaways Running Smoothly`, `ZeroSync on YouTube`];
+  const activities = [`Giveaways Running`, `Spiderman (Ads)`];
   setInterval(() => {
     let activity = activities[Math.floor(Math.random() * activities.length)];
     client.user.setActivity(activity, { type: "WATCHING" });
